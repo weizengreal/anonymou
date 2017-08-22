7eb1434656bebe6e2013a804dc4e94ae32b5d6c6
weizeng <466730846@qq.com>
2017-08-22 22:03:24 +0800

    store a new version & store vender to github

--- application/database.php
+++ application/database.php
@@ -17,9 +17,9 @@ return [
     // 数据库名
     'database'        => 'anonymou',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'woai662',
     // 密码
-    'password'        => 'root',
+    'password'        => 'weiwei66291',
     // 端口
     'hostport'        => '',
     // 连接dsn
